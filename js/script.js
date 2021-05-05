e68fc6760a15887e0c1b9006594ffa8868b4a974
Valera Feofanov <evospider2099@gmail.com>
2021-05-05 10:35:18 +0300

    Second homework change

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@ for (let i = 0; i < 2; i++) {
     const a = prompt('Один из последних просмотренных фильмов?', ''),
           b = +prompt('На сколько оцените его?', '');
 
-    if (a != null && a != '' && a.length < 50) {
+    if (a != null && b != null && a != '' && b != '' && a.length < 50) {
         personalMovieDB.movies[a] = b;
     } else {
         i--;
